2fb65f9783072570071ee752915dcd03b91c1b8b
thanooshakarri <thanooshakarri@arizona.edu>
2023-12-14 15:02:17 -0700

    final result

--- README.md
+++ README.md
@@ -6,8 +6,35 @@ Task URL: https://www.kaggle.com/c/ling-582-fa-2023-course-competition
 
 Invitation URL: https://www.kaggle.com/t/f43bfaa8c4eff2644b60e83ef065c7e3
 
-# Notes
-- You are not obligated to use Python
-- You may delete or alter any files in this repository
-- You are free to add dependencies
-  - Ensure that your code can be installed/used on another machine running Linux or MacOS (consider containerizing your project with Docker or an equivalent technology)
+
+Given two spans of text, determine if both spans were produced by the same author.
+
+This repository contains a trained model that classifies text, if both the texts are by the same author or not. My neural network is trained by using distil-RoBERTa embedding in a Bi-directional Gated Recurrent Unit.
+
+Files:
+
+├── README.md             <- Introduction of repository
+├── model                 <- Trained model
+├── requirements.txt      <- Python packages requirement file
+├── data                  <- Dataset
+|   |____ train.csv       <- Train data
+|   |____ test.csv        <- Test data
+├── src                   <- Source code
+|   |____ Classifier.py   <- Neural Network
+|____ Main.ipynb         <- main function 
+
+Usage:
+
+Install the required libraries listed in requirements.txt by running pip install -r requirements.txt in your terminal.
+
+Open the Main.ipynb notebook in Jupyter or any compatible platform.
+
+The trained model is in the model folder.
+
+The Classifier.py contains the files to train the model.
+
+To make predictions upload the file namming it test.csv.
+
+The test file should contain a column TEXT with the text.
+
+The model is loaded and predicttions are made and the results are saved into predictions.csv file
--- scripts/Classifier.py
+++ scripts/Classifier.py
@@ -4,7 +4,9 @@ import numpy
 import transformers
 import datasets
 import tensorflow as tf
-from sklearn.metrics import multilabel_confusion_matrix
+from sklearn.metrics import confusion_matrix
+from nltk import pos_tag
+from nltk.tokenize import word_tokenize
 
 # use the tokenizer from DistilRoBERTa
 tokenizer = transformers.AutoTokenizer.from_pretrained("distilroberta-base")
@@ -26,54 +28,97 @@ class Classifier():
         pd.concat([X_train,y_train],axis=1).to_csv("data/train.csv",index=False)
         pd.concat([X_test,y_test],axis=1).to_csv("data/test.csv",index=False)
         pd.concat([X_dev,y_dev],axis=1).to_csv("data/dev.csv",index=False)
+    
+    def split(sef, text):
+        a, b=text["TEXT"].split("[SNIPPET]")
+        return {"one":a,"two":b}
 
     def tokenize(self,examples):
         #tokenizing using bert
         return tokenizer(examples["TEXT"], truncation=True, max_length=64,
                         padding="max_length",return_tensors="tf")
+    def count_punctuation(self, example):
+        punctuation = ["?", ",", ";", ".", ":", "'", '"', "!", "-"]
+        
+        # Count punctuation for 'one' column
+        one_text = example['one']
+        one_punctuation_count = {f'one_{punct}_count': one_text.count(punct) for punct in punctuation}
+        
+        # Count punctuation for 'two' column
+        two_text = example['two']
+        two_punctuation_count = {f'two_{punct}_count': two_text.count(punct) for punct in punctuation}
+        # Merge counts into example dictionary
+        example.update({"ONE":one_punctuation_count.values()})
+        example.update({"TWO":two_punctuation_count.values()})
+        
+        return example
+    def get_pos_tags(self, sentence):
+        tokens = word_tokenize(sentence["one"])
+        pos_tags = pos_tag(tokens)
+        one_pos=' '.join(t[1] for t in pos_tags)
+        tokens = word_tokenize(sentence["two"])
+        pos_tags = pos_tag(tokens)
+        two_pos=' '.join(t[1] for t in pos_tags)
+        limiters = ["DT", "IN", "CC","PRD","MD"]
+        one_punctuation_count = {f'one_{punct}_count': one_pos.count(punct) for punct in limiters}
+        two_punctuation_count = {f'one_{punct}_count': two_pos.count(punct) for punct in limiters}
+        sentence.update({"one_pos":one_punctuation_count.values()})
+        sentence.update({"two_pos":two_punctuation_count.values()})
+        return sentence
+
     
 
     def fit(self, train_path="data/train.csv", devlopment_path="data/dev.csv"):
         # load the CSVs into Huggingface datasets
-        self.humorous_headlines_dataset = datasets.load_dataset("csv", data_files={
+        self.dataset = datasets.load_dataset("csv", data_files={
             "train": train_path, "validation": devlopment_path})
         #traines the model using bidirectional neural network layer and saves the model into a model file
         self.dataset = self.dataset.map(self.tokenize, batched=True)
-
-        train = self.dataset["train"].to_tf_dataset(
-            columns="input_ids",
-            label_cols="LABEL",
-            batch_size=16,
-            shuffle=True)
-        devlopment = self.dataset["validation"].to_tf_dataset(
-            columns="input_ids",
-            label_cols="LABEL",
-            batch_size=16)
+        self.dataset = self.dataset.map(self.split)
+        self.dataset = self.dataset.map(self.count_punctuation)
+        self.dataset = self.dataset.map(self.get_pos_tags)
+        pos=tf.convert_to_tensor(self.dataset["train"]["ONE"])
+        pso=tf.convert_to_tensor(self.dataset["train"]["TWO"])
+        text=tf.convert_to_tensor(self.dataset["train"]["input_ids"])
+        label=tf.expand_dims(tf.convert_to_tensor(self.dataset["train"]["LABEL"]), axis=1)
+        label_train=tf.cast(
+        label, dtype=tf.float32)
+        train_dataset=tf.concat([text, pos, pso, tf.convert_to_tensor(self.dataset["train"]["one_pos"]), tf.convert_to_tensor(self.dataset["train"]["two_pos"])],1)
+        pos=tf.convert_to_tensor(self.dataset["validation"]["ONE"])
+        pso=tf.convert_to_tensor(self.dataset["validation"]["TWO"])
+        text=tf.convert_to_tensor(self.dataset["validation"]["input_ids"])
+        label=tf.expand_dims(tf.convert_to_tensor(self.dataset["validation"]["LABEL"]), axis=1)
+        label_dev=tf.cast(
+        label, dtype=tf.float32)
+        dev_dataset=tf.concat([text, pos, pso, tf.convert_to_tensor(self.dataset["validation"]["one_pos"]), tf.convert_to_tensor(self.dataset["validation"]["two_pos"])],1)
+        # convert Huggingface datasets to Tensorflow datasets
         early_stop=tf.keras.callbacks.EarlyStopping(
-            monitor="val_loss",
+            monitor="val_f1_score",
             patience=2,
-            mode="min",
-            restore_best_weights=True
+            mode="max"
         )
+        #initializer = tf.keras.initializers.RandomNormal(mean=0.,stddev=0.01)
         model = tf.keras.Sequential()
-        model.add(tf.keras.layers.Embedding(tokenizer.vocab_size,4 ,embeddings_initializer="RandomUniform"))
-        model.add(tf.keras.layers.Bidirectional(tf.keras.layers.GRU(4)))
-        model.add(tf.keras.layers.Dense(4))
+        model.add(tf.keras.layers.Embedding(tokenizer.vocab_size,8))
+        model.add(tf.keras.layers.Bidirectional(tf.keras.layers.GRU(8, return_sequences=True)))
+        model.add(tf.keras.layers.Bidirectional(tf.keras.layers.GRU(8)))
+        model.add(tf.keras.layers.Dense(16))
         model.add(tf.keras.layers.Flatten())
         model.add(tf.keras.layers.Dense(
                 1,
                 activation='sigmoid'
                 ))
         model.compile(
-            optimizer=tf.keras.optimizers.Adam(0.001),
+            optimizer=tf.keras.optimizers.Adam(0.001, clipnorm=1.0),
             loss=tf.keras.losses.binary_crossentropy,
             metrics=[tf.keras.metrics.F1Score(threshold=0.5)])
         # fit the model to the training data, monitoring F1 on the dev data
         model.fit(
-            train,
+            train_dataset,
+            label_train,
             epochs=10,
             batch_size=16,
-            validation_data=devlopment,
+            validation_data=(dev_dataset,label_dev),
             callbacks=[
                 tf.keras.callbacks.ModelCheckpoint(
                     filepath="model",
@@ -82,6 +127,8 @@ class Classifier():
                     save_best_only=True),early_stop],
                     class_weight={0:0.89,
                                 1:2.079})
+
+
     
     def save_to_file(self,path="data/predict_scores.csv"):
         #takes the data makes predicions and saves them into a predictions.csv file 
@@ -90,7 +137,7 @@ class Classifier():
         test_data = pd.read_csv(path)
         predictions=self.predict(path)
         predictions=predictions.reshape((1,-1))[0]
-        pd.concat([test_data[["ID"]],pd.DataFrame({"LABEL":predictions})],axis=1).to_csv("data/predictions.csv")
+        pd.concat([test_data[["ID"]],pd.DataFrame({"LABEL":predictions})],axis=1).to_csv("data/predictions.csv", index=False)
 
     def predict(self,path="data/test.csv"):
         model = tf.keras.models.load_model("model")
@@ -101,14 +148,20 @@ class Classifier():
         # create input features in the same way as in train()
         self.dataset = datasets.Dataset.from_pandas(test_data)
         self.dataset = self.dataset.map(self.tokenize, batched=True)
-        
-        tf_dataset = self.dataset.to_tf_dataset(
-            columns="input_ids",
-            batch_size=16)
+        self.dataset = self.dataset.map(self.split)
+        self.dataset = self.dataset.map(self.count_punctuation)
+        self.dataset = self.dataset.map(self.get_pos_tags)
+        self.dataset = self.dataset.map(self.split)
+        self.dataset = self.dataset.map(self.count_punctuation)
+        self.dataset = self.dataset.map(self.get_pos_tags)
+        pos=tf.convert_to_tensor(self.dataset["ONE"])
+        pso=tf.convert_to_tensor(self.dataset["TWO"])
+        text=tf.convert_to_tensor(self.dataset["input_ids"])
+        tf_dataset=tf.concat([text, pos, pso, tf.convert_to_tensor(self.dataset["one_pos"]), tf.convert_to_tensor(self.dataset["two_pos"])],1)
         
         return numpy.where(model.predict(tf_dataset) > 0.5, 1, 0)
     
     def confusion_matrix(self,path="data/test.csv"):
         test_data=pd.read_csv(path)
         predictions=self.predict(path)
-        print(multilabel_confusion_matrix(predictions,test_data[["LABEL"]]))
+        print(confusion_matrix(predictions,test_data[["LABEL"]]))
